59ddba8f6e653423484b0effb5e8d36984858b12
praveen-vp <praveen_v.p@outlook.com>
2018-10-06 18:17:06 +0530

    adding index page closes #1

--- src/main/java/com/pvp/recipe/recipeproject/RecipeProjectApplication.java
+++ src/main/java/com/pvp/recipe/recipeproject/RecipeProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipeProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecipeProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeProjectApplication.class, args);
+    }
 }
