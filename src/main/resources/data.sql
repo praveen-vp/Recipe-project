5cd53d9ad889b06e8b6caff9f50add17120df1bf
praveen-vp <praveen.vp@inxsasia.com>
2018-11-25 15:09:03 +0530

    Adding data loader sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into category (description) values ('Indian');
+insert into category (description) values ('South Indian');
+insert into category (description) values ('North Indian');
+insert into category (description) values ('Tandoor - Indian');
+insert into unit_of_measure (description) values ('Teaspoon');
+insert into unit_of_measure (description) values ('pinch');
+insert into unit_of_measure (description) values ('Cup');
+insert into unit_of_measure (description) values ('Tablespoon');
