d28d651d12ce865306a1384e0cc578afa2e74ad4
praveen-vp <praveen.vp@inxsasia.com>
2019-01-15 16:27:16 +0530

    Adding lombok log to service

--- src/main/java/com/pvp/recipe/service/RecipeServiceImp.java
+++ src/main/java/com/pvp/recipe/service/RecipeServiceImp.java
@@ -2,11 +2,13 @@ package com.pvp.recipe.service;
 
 import com.pvp.recipe.model.Recipe;
 import com.pvp.recipe.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImp implements RecipeService {
 
@@ -18,6 +20,8 @@ public class RecipeServiceImp implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+
+        log.debug("inside getRecipes in RecipeServiceImp");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
